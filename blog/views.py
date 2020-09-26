1f7718481dd0f45690c3a46bcf91422d201cd084
wedevelopers <wedevelopers404@gmail.com>
2020-09-26 15:03:45 +0530

    urls added

--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse('Have to write cbv instead of fbv')
+def contact(request):
+    return HttpResponse('This is Home Page')
