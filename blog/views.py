54f4010899cac417fc99b7b2ac1773bc7e8aa743
Sujay Shekhar <sujayshekhar162@gmail.com>
2020-09-27 19:48:59 +0530

    blog/views.py homefunction changed

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return HttpResponse('Have to write cbv instead of fbv')
+    return HttpResponse('<h1>This is Home Page of Blog</h1>')
 def contact(request):
     return HttpResponse('This is Contact Page I am Prakash')
