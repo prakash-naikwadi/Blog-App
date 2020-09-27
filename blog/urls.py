10757d640dd01a11dd41cab1c41d26e03aba9489
Sujay Shekhar <sujayshekhar162@gmail.com>
2020-09-27 21:34:45 +0530

    serving static files and temps

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from .import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     
     path('',views.home, name = 'BlogHome'),
     path('contact/',views.contact, name = 'BlogContact'),
-]
+]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
